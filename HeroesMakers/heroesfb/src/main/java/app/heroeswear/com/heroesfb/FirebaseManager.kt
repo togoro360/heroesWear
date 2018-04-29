f8308c3bbd7ef08d05b1becc2520a5ebe974e9de
livnat avikasis <livnata@gett.com>
2018-04-29 10:28:42 +0300

    first commit

---
+++ HeroesMakers/heroesfb/src/main/java/app/heroeswear/com/heroesfb/FirebaseManager.kt
@@ -0,0 +1,20 @@
+package app.heroeswear.com.heroesfb
+
+/**
+ * Created by livnatavikasis on 29/04/2018.
+ */
+class FirebaseManager(){
+
+//    lateinit mAuth: FirebaseAuth
+//    fun onCreate(){
+//        mAuth = FirebaseAuth.getInstance();
+//
+//    }
+//
+//    fun onStart(){
+//                 // Check if user is signed in (non-null) and update UI accordingly.
+//            currentUser: FirebaseUser = mAuth.getCurrentUser();
+//            updateUI(currentUser);
+//
+//    }
+}
