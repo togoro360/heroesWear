da0b48d544dbbd1941d2872f6881f12a1eaa73f8
Shirly Kadosh <shirlyk@gett.com>
2018-05-01 14:20:31 +0300

    Add notifications builder

---
+++ HeroesMakers/app/src/main/java/app/heroeswear/com/heroesmakers/login/application/App.java
@@ -0,0 +1,25 @@
+package app.heroeswear.com.heroesmakers.login.application;
+
+import android.app.Application;
+import android.os.Build;
+
+import app.heroeswear.com.heroesmakers.login.enums.NotificationChannelType;
+import app.heroeswear.com.heroesmakers.login.utils.NotificationFactory;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannels();
+    }
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            for (NotificationChannelType channelType : NotificationChannelType.values()) {
+                NotificationFactory.createChannel(this, channelType);
+            }
+        }
+    }
+}
